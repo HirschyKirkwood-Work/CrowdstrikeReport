0516483ac038d4c728b247c5a0fa99a5079f8cf7
Hirschy Kirkwood <hkirkwoo@andrew.cmu.edu>
2023-09-21 14:00:41 -0400

    Refactored to use dicts for dsp data and added ABR checkin time

--- falcon_info.py
+++ falcon_info.py
@@ -14,10 +14,14 @@ class FalconInfo:
     def get_ldapinfo(self,pth:str):
         with open(pth, 'r') as file2:
             for line in file2:
+
                 columns = line.strip().split(',')
-                andrewID = columns[0]
+                andrewID = columns[0].lower()
                 try:
                     department = columns[2]
+                    if department == '':
+                        department = "No LDAP info"
+                        
                 except:
                     department = "No LDAP info"
                 self.andrewID_dict[andrewID] = department
@@ -36,8 +40,17 @@ class FalconInfo:
             for line in file3:
                 columns = line.strip().split(',')
                 dsp_hostname = columns[0]
-                dsp_andrewid = columns[1]
-                self.dsp_data_dict[dsp_hostname] = dsp_andrewid
+                dsp_last_seen = columns[2]
+                dsp_andrewid = columns[18].lower()
+
+                # Check if the andrewid is already in the dictionary
+                if dsp_hostname not in self.dsp_data_dict:
+                    self.dsp_data_dict[dsp_hostname] = {}  # Create a new dictionary if not exists
+
+                # Add or update data for the andrewid
+                self.dsp_data_dict[dsp_hostname]["andrewID"] = dsp_andrewid
+                self.dsp_data_dict[dsp_hostname]["hostname"] = dsp_hostname
+                self.dsp_data_dict[dsp_hostname]["last_seen"] = dsp_last_seen
 
     def process_info(self, dsp_infopath: str, ldap_infopath: str, falcon_path:str, outpath: str, inactive_out: str):
         self.get_dspinfo(dsp_infopath)
@@ -46,23 +59,26 @@ class FalconInfo:
         with open(outpath, 'w') as outfile:
             outfile.write("Hostname,AndrewID,Last Seen,Department\n")
         with open(inactive_out, 'w') as inactive:
-            inactive.write("Hostname,AndrewID,Department\n")
+            inactive.write("Hostname,AndrewID,Department,Last Seen\n")
         with open(outpath, 'a') as outfile:
             with open(inactive_out, "a") as inactive:
+                andrew_ids = [value["andrewID"] for value in self.dsp_data_dict.values()]
                 for hostname, andrewID in self.dsp_data_dict.items():
                     # Check if dsp_hostname matches hostname and dsp_andrewid is in andrewID_dict
-                    if hostname in self.falcon_data_dict and andrewID in self.andrewID_dict:
-                        department = self.andrewID_dict[andrewID].replace('"','')
+                    if hostname in self.falcon_data_dict and andrewID["andrewID"] in self.andrewID_dict:
+                        department = self.andrewID_dict[andrewID['andrewID']].replace('"','')
                         last_seen = self.falcon_data_dict[hostname]
-                        outfile.write(f"{hostname},{andrewID},{last_seen[:10]},{department}\n")
+                        outfile.write(f"{hostname},{andrewID['andrewID']},{last_seen[:10]},{department}\n")
                     # Write users with computer who aren't in Falcon
-                    elif andrewID in self.andrewID_dict and hostname:
-                        department = self.andrewID_dict[andrewID].replace('"','')
-                        inactive.write(f"{hostname},{andrewID},{department}\n")
-                    # Write users who don't have computers on record w/ DSP
+                    elif andrewID['andrewID'] in self.andrewID_dict and hostname:
+                        department = self.andrewID_dict[andrewID['andrewID']].replace('"','')
+                        inactive.write(f"{hostname},{andrewID['andrewID']},{department},{andrewID['last_seen']}\n")
+                    # else:# Non-Falcon users
+                    #     print(andrewID['andrewID'], hostname)
+                # Write users who don't have computers on record w/ DSP
                 for andrewID, department in self.andrewID_dict.items():
-                    if andrewID not in self.dsp_data_dict.values():
-                        inactive.write(f"Not A Customer or no machine on SLA,{andrewID},{department}\n")
+                    if andrewID not in andrew_ids:
+                        inactive.write(f"Not A Customer or no machine on SLA,{andrewID},{department},N/A\n")
                     
 if __name__ == '__main__':
     falcon = FalconInfo()
